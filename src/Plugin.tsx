deb1a72aa8bdd88915022a64813515543b16a52c
Kyle <kmacdonald1983@gmail.com>
2022-02-03 11:04:32 -0600

    initial test.

---
+++ src/Plugin.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import * as Flex from '@twilio/flex-ui';
+import { FlexPlugin } from 'flex-plugin';
+
+
+
+const PLUGIN_NAME = '{{pluginClassName}}';
+
+export default class TypeScriptDemoPlugin extends FlexPlugin {
+  constructor() {
+    super(PLUGIN_NAME);
+  }
+
+  /**
+   * This code is run when your plugin is being started
+   * Use this to modify any UI components or attach to the actions framework
+   *
+   * @param flex { typeof Flex }
+   * @param manager { Flex.Manager }
+   */
+  async init(flex: typeof Flex, manager: Flex.Manager): Promise<void> {
+    console.log('hell world');
+  }
+}
