6a9db3326307ecbb37d2d70e8830cbb068b65710
James McCollum <jmccollum@roydan.com>
2023-01-23 11:25:56 -0600

    Initial cleanup

--- plugin/src/Plugin.tsx
+++ plugin/src/Plugin.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import * as Flex from '@twilio/flex-ui';
-import { FlexPlugin } from '@twilio/flex-plugin';
+import React from "react";
+import * as Flex from "@twilio/flex-ui";
+import { FlexPlugin } from "@twilio/flex-plugin";
 
-
-
-const PLUGIN_NAME = '{{pluginClassName}}';
+const PLUGIN_NAME = "{{pluginClassName}}";
 
 export default class TypeScriptDemoPlugin extends FlexPlugin {
-  constructor() {
-    super(PLUGIN_NAME);
-  }
+	constructor() {
+		super(PLUGIN_NAME);
+	}
 
-  /**
-   * This code is run when your plugin is being started
-   * Use this to modify any UI components or attach to the actions framework
-   *
-   * @param flex { typeof Flex }
-   * @param manager { Flex.Manager }
-   */
-  async init(flex: typeof Flex, manager: Flex.Manager): Promise<void> {
-    console.log('hell world');
-  }
+	/**
+	 * This code is run when your plugin is being started
+	 * Use this to modify any UI components or attach to the actions framework
+	 *
+	 * @param flex { typeof Flex }
+	 * @param manager { Flex.Manager }
+	 */
+	async init(flex: typeof Flex, manager: Flex.Manager): Promise<void> {
+		console.log("hell world");
+	}
 }
